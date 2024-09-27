cf35ee86c39a2eb4debf57b50e0a05bb06132689
Al-Am123 <aluno_manha1234@hotmail.com>
2024-09-27 10:07:04 -0300

    feat: add json dados do pedido

--- Trabalho cateq/servidor.py
+++ Trabalho cateq/servidor.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
+dados_armazenados = {}
 
 @app.route("/mesa/<int:mesa_id>")
 def mostrar_cardapio(mesa_id):
@@ -13,6 +14,18 @@ def confirmacao_pedido():
     mesa_id = request.args.get("mesa_id")
     return render_template("confirmacao.html", mesa_id=mesa_id,)
 
+
+@app.route('/api/dados', methods=['POST'])
+def api_dados():
+    dados = request.json
+    dados_armazenados['dados'] = dados.get('dados')
+
+    return jsonify(dados), 200
+
+
+@app.route('/api/dados', methods=['GET'])
+def get_dados():
+    return jsonify(dados_armazenados), 200
 
 if __name__ == "__main__":
     app.run(debug=True, port=3311)
